4d028402efb9c94960452cfaa564ce50dfd2db5c
superpikapool13 <189666978+superpikapool13@users.noreply.github.com>
2026-08-09 15:39:55 +0530

    Clean auto-generated files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { vitePluginHtmlMinify } from 'vite-plugin-html-minify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginHtmlMinify()
+  ]
 })
