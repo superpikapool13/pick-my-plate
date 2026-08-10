70b6c07a5c3cd68111a490c15a575ef0ff50bf9c
superpikapool13 <189666978+superpikapool13@users.noreply.github.com>
2026-08-10 22:33:44 +0530

    Add cuisine list - List is stored in `cuisines.js` with name and imagepath   - Image path is obtained from barrel `image.js`     which import paths and exports as object

---
+++ src/components/cuisines/cuisines.js
@@ -0,0 +1,116 @@
+import * as images from './images/images';
+
+export const cuisines = [
+    {
+        name:   "Pizza",
+        img:    images.pizza
+    },
+    {
+        name:   "Burger",
+        img:    images.burger
+    },
+    {
+        name:   "Biryani",
+        img:    images.biryani
+    },
+    {
+        name:   "Sandwich",
+        img:    images.sandwich
+    },
+    {
+        name:   "Rolls",
+        img:    images.rolls
+    },
+    {
+        name:   "North Indian",
+        img:    images.northindian
+    },
+    {
+        name:   "Chaat",
+        img:    images.chaat
+    },
+    {
+        name:   "Thali",
+        img:    images.thali
+    },
+    {
+        name:   "Dosa",
+        img:    images.dosa
+    },
+    {
+        name:   "South Indian",
+        img:    images.southindian
+    },
+    {
+        name:   "Shawarma",
+        img:    images.shawarma
+    },
+    {
+        name:   "Tiramisu",
+        img:    images.tiramisu
+    },
+    {
+        name:   "Fried Rice",
+        img:    images.friedrice
+    },
+    {
+        name:   "Chinese",
+        img:    images.chinese
+    },
+    {
+        name:   "Cake",
+        img:    images.cake
+    },
+    {
+        name:   "Aloo Paratha",
+        img:    images.paratha
+    },
+    {
+        name:   "Chicken Lollipop",
+        img:    images.chickenlollipop
+    },
+    {
+        name:   "Desserts",
+        img:    images.desserts
+    },
+    {
+        name:   "Coffee",
+        img:    images.coffee
+    },
+    {
+        name:   "Momos",
+        img:    images.momos
+    },
+    {
+        name:   "Noodles",
+        img:    images.noodles
+    },
+    {
+        name:   "Tea",
+        img:    images.tea
+    },
+    {
+        name:   "Pastry",
+        img:    images.pastry
+    },
+    {
+        name:   "Salad",
+        img:    images.salad
+    },
+    {
+        name:   "Waffle",
+        img:    images.waffle
+    },
+    {
+        name:   "Pasta",
+        img:    images.pasta
+    },
+    {
+        name:   "Khichdi",
+        img:    images.khichdi
+    },
+    {
+        name:   "Ice Cream",
+        img:    images.icecream
+    },
+]
---
+++ src/components/cuisines/images/images.js
@@ -0,0 +1,62 @@
+//Helper ("barrel") file for intermediate image importing
+
+import biryani from "./biryani.webp"
+import burger from "./burger.webp"
+import cake from "./cake.webp"
+import chaat from "./chaat.webp"
+import chickenlollipop from "./chicken_lollipop.webp"
+import chinese from "./chinese.webp"
+import coffee from "./coffee.webp"
+import desserts from "./desserts.webp"
+import dosa from "./dosa.webp"
+import friedrice from "./fried_rice.webp"
+import icecream from "./ice_cream.webp"
+import khichdi from "./khichdi.webp"
+import momos from "./momos.webp"
+import noodles from "./noodles.webp"
+import northindian from "./north_indian.webp"
+import paratha from "./paratha.webp"
+import pasta from "./pasta.webp"
+import pastry from "./pastry.webp"
+import pizza from "./pizza.webp"
+import rolls from "./rolls.webp"
+import salad from "./salad.webp"
+import sandwich from "./sandwich.webp"
+import shawarma from "./shawarma.webp"
+import southindian from "./south_indian.webp"
+import tea from "./tea.webp"
+import thali from "./thali.webp"
+import tiramisu from "./tiramisu.webp"
+import waffle from "./waffle.webp"
+
+export {
+    biryani,
+    burger,
+    cake,
+    chaat,
+    chickenlollipop,
+    chinese,
+    coffee,
+    desserts,
+    dosa,
+    friedrice,
+    icecream,
+    khichdi,
+    momos,
+    noodles,
+    northindian,
+    paratha,
+    pasta,
+    pastry,
+    pizza,
+    rolls,
+    salad,
+    sandwich,
+    shawarma,
+    southindian,
+    tea,
+    thali,
+    tiramisu,
+    waffle,
+
+};
