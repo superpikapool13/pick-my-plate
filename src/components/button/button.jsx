efc077c7c970a0efa432d2d18886a8045f56008f
superpikapool13 <189666978+superpikapool13@users.noreply.github.com>
2026-08-11 18:40:47 +0530

    Add button component

---
+++ src/components/button/button.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+import styles from "./button.module.css";
+
+export default function Button(props) {
+    return (
+        <button
+            onClick={props.handler}
+            className={styles.button}
+        >
+            Choose!
+        </button>
+    )
+}
