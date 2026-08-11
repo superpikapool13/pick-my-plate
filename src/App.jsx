1c4b151566a1139341078ac4e0fbf2fd783c463d
superpikapool13 <189666978+superpikapool13@users.noreply.github.com>
2026-08-11 15:54:56 +0530

    Add header component - From superpikapool/SVG-to-PNG@093c1f9115f5a3e761b7898780de8e98ab157185   originally from jamesjarvis/git-art@e35471e10cba61b92fe0dd3991b88b120194e04b#diff-b9b2b997906c030374ac927d0ae375be39da0e694f96304015c36b271f8902d6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './assets/App.module.css'
+import { cuisines } from './components/cuisines/cuisines'
+import Header from './components/header/Header'
+import FoodIcon from './components/foodIcon/foodIcon'
 
 export default function App() {
 
     return (
         <>
+            <Header title="Hello" />
+
+            {cuisines.map((cuisine) => (
+                <FoodIcon
+                    name={cuisine.name}
+                    image={cuisine.img}
+                />
+            ))}
         </>
     )
 }
